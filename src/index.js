9a60bac0e1a41d25504b91edfbdf20b62da07d17
plavinsky <49480393+plavinsky@users.noreply.github.com>
2021-09-09 19:26:21 +0300

    Second Task
    elements, components, css-modules, import/export classes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import HeaderBlock from "./components/HeaderBlock";
+
+import './index.css'
+
+// const el = React.createElement(
+//   'h1',
+//   null,
+//   'Hello World, ReactJS!'
+// );
+
+const AppList = () => {
+  const items = ['Item1', 'Item2', 'Item3', 'Item4'];
+  const fisrtItems = <li>Item0</li>
+
+  const isAuth = false;
+
+  return (
+    <ul>
+      {
+        isAuth ? fisrtItems : null
+      }
+      {
+        items
+      }
+      {
+        items.map(item => <li>{item}</li>)
+      }
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+  </ul>
+  )
+}
+
+const AppHeader = () => {
+  const margin = 40;
+  
+  const headerStyle = {
+    color: 'red',
+    marginLeft: `${margin}px`,
+    marginBottom: `${margin}px`
+  };
+  return (
+  <h1
+      className="header"
+      >
+        Hello, World!
+  </h1>
+  )
+}
+
+const AppInput = () => {
+  const placeholder = "Type text...";
+
+  return (
+    <label>
+      <input 
+      id="search"
+      placeholder={placeholder}
+       />
+    </label>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <HeaderBlock />
+      <AppHeader />
+      <AppList />
+      <AppHeader />
+      <AppList />
+      <AppHeader />
+      <AppList />
+      <AppInput />
+    </>
+  );
+}
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
