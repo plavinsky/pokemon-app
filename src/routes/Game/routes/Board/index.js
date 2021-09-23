4d42da3b1c94f6dac8fb7d0b907e000a197fe936
plavinsky <49480393+plavinsky@users.noreply.github.com>
2021-09-23 20:04:46 +0300

    from videos
    Create Board
    Create Player2

--- src/routes/Game/routes/Board/index.js
+++ src/routes/Game/routes/Board/index.js
@@ -6,16 +6,22 @@ import s from './style.module.css';
 
 const BoardPage = () => {
     const [board, setBoard] = useState([]);
+    const [player2, setPlayer2] = useState([]);
     const {pokemon} = useContext(PokemonContext);
     const history = useHistory();
+    
 
     useEffect(async () => {
-         const boardResponse = await fetch("https://reactmarathon-api.netlify.app/api/board");
+        const boardResponse = await fetch("https://reactmarathon-api.netlify.app/api/board");
+        const boardRequest = await boardResponse.json();
+        setBoard(boardRequest.data);
+         
+        const player2Response = await fetch("https://reactmarathon-api.netlify.app/api/create-player");
+        const player2Request =  await player2Response.json();
 
-         const boardRequest = await boardResponse.json();
-         console.log("1:", boardRequest.data)
-         setBoard(boardRequest.data);
-         console.log("2:", board);
+        console.log("p2_data:", player2Request.data);
+        setPlayer2(player2Request.data);
+
     }, [])
 
     // if (Object.values(pokemon).length < 5)
@@ -62,10 +68,30 @@ const BoardPage = () => {
                 }
 
 
-
+           
 
 
                
+            </div>
+
+            <div className={s.playerTwo}>
+            {
+                    player2.map(
+                        ({name, id, img, type, values, selected}) => (
+                        <PokemonCard й
+                        className={s.card}
+                        key={id} 
+                        dbKey={id}
+                        name={name} 
+                        id={id} 
+                        img={img} 
+                        type={type} 
+                        values={values} 
+                        active={true} 
+                        isSelected={selected}
+                        minimize/>
+                    ) )
+                }  
             </div>
         </div>
     );
