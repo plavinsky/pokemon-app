ba2eed300c36446a87a8a78e904bfe59ffd60fdf
plavinsky <49480393+plavinsky@users.noreply.github.com>
2021-10-09 00:25:37 +0300

    fix logo and menu bug

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import {ReactComponent as LoginSVG} from "../../assets/login.svg";
 import {ReactComponent as UserSVG} from "../../assets/user.svg";
 import { selectLocalID, selectUserIsLoading } from "../../store/user";
+import {useHistory} from 'react-router';
 
 import s from "./style.module.css";
 
@@ -11,7 +12,7 @@ import s from "./style.module.css";
 
 
 const Navbar = ({onMenuClick, isActive, bgActive = false, onClickLogin}) => {
-
+    const history = useHistory();
     const isLoading = useSelector(selectUserIsLoading);
     const localId = useSelector(selectLocalID);
 
@@ -19,9 +20,9 @@ const Navbar = ({onMenuClick, isActive, bgActive = false, onClickLogin}) => {
     //     onMenuClick && onMenuClick();
     // }
 
-    // const handleLogoClick = () => {
-    //     history.push('');
-    // }
+    const handleLogoClick = () => {
+        history.push('');
+    }
 
 
 
@@ -29,7 +30,7 @@ const Navbar = ({onMenuClick, isActive, bgActive = false, onClickLogin}) => {
         <nav id={s.navbar} className={cn({[s.bgActive]: bgActive})}>
         <div className={s.navWrapper}>
             <div>
-                <p className={s.brand} onClick={onMenuClick}>
+                <p className={s.brand} onClick={handleLogoClick}>
                 LOGO
                 </p>
             </div>
